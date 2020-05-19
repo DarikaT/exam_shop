d24b8a898f119d1227251ae666c75fadb71e1bea
DarikaT <darika.taranchieva@gmail.com>
2020-05-19 06:08:58 +0600

    changes in search bar

--- shop/views.py
+++ shop/views.py
@@ -3,29 +3,28 @@ from django.views.generic import ListView, DetailView
 from .models import Product, Category
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .mixins import GetQuerySetMixin, DropdownMixin
+from django.db.models import Q
 
 class ProductListView(GetQuerySetMixin, DropdownMixin, ListView):
     model = Product 
     template_name = 'product_list.html'
     paginate_by = 6
-    ordering = ('-date',)
 
+    def get_queryset(self):
+        query_result = self.request.GET.get('q')
+        if query_result: 
+            queryset = Product.objects.filter(
+                Q(name__icontains=query_result)|
+                Q(price__icontains=query_result)
+            )
+        else:
+            queryset = Product.objects.all()
+        return queryset
 
-
-class ProductDetailView(DropdownMixin, DetailView):
+class ProductDetailView(GetQuerySetMixin, DropdownMixin, DetailView):
     model = Product
     template_name = 'product_detail.html'
 
-class Search(ListView):
-    paginate_by = 3
-    def get_queryset(self):
-        return Product.objects.filter(name__icontains=self.request.GET.get('q'))
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['q'] = self.request.GET.get('q')
-        return context
-
-    
 
 
